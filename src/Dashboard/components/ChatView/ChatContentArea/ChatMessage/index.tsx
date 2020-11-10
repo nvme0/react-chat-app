924099f1448c192d66fe19368601a9784c36ecc4
nvme0 <jeff@solutions4theweb.com>
2020-11-11 07:54:51 +1000

    Add support for Video Uploads to the chat

--- src/Dashboard/components/ChatView/ChatContentArea/ChatMessage/index.tsx
+++ src/Dashboard/components/ChatView/ChatContentArea/ChatMessage/index.tsx
@@ -1,12 +1,37 @@
 import React from "react";
+import ReactPlayer from "react-player";
+
 import { Message } from "../../../../../datastoreTypes";
+import useStyles from "./styles";
 
 const ChatMessage = ({ content, contentType }: Message) => {
+  const classes = useStyles();
+
   switch (contentType) {
+    case "image/apng":
+    case "image/avif":
+    case "image/gif":
     case "image/jpeg":
     case "image/png":
+    case "image/svg+xml":
+    case "image/webp":
+    case "image/bmp":
+    case "image/x-icon":
+    case "image/tiff":
+      return <img src={content} className={classes.image} loading="lazy" />;
+
+    case "video/3gpp":
+    case "video/3gpp2":
+    case "video/3gp2":
+    case "video/mpeg":
+    case "video/mp4":
+    case "video/ogg":
+    case "video/quicktime":
+    case "video/webm":
       return (
-        <img src={content} style={{ maxHeight: "180px" }} loading="lazy" />
+        <div className={classes.videoPlayerContainer}>
+          <ReactPlayer url={content} controls light />
+        </div>
       );
 
     default:
--- src/datastoreTypes.ts
+++ src/datastoreTypes.ts
@@ -1,4 +1,43 @@
-export type ContentType = "image/jpeg" | "image/png";
+// source: https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types
+export type ContentTypeImage =
+  | "image/apng"
+  | "image/avif"
+  | "image/gif"
+  | "image/jpeg"
+  | "image/png"
+  | "image/svg+xml"
+  | "image/webp"
+  | "image/bmp"
+  | "image/x-icon"
+  | "image/tiff";
+
+// source: https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Containers
+export type ContentTypeVideo =
+  | "video/3gpp"
+  | "video/3gpp2"
+  | "video/3gp2"
+  | "video/mpeg"
+  | "video/mp4"
+  | "video/ogg"
+  | "video/quicktime"
+  | "video/webm";
+export type ContentTypeAudio =
+  | "audio/3gpp"
+  | "audio/3gpp2"
+  | "audio/3gp2"
+  | "audio/aac"
+  | "audio/mpeg"
+  | "audio/flac"
+  | "audio/x-flac"
+  | "audio/mp4"
+  | "audio/ogg"
+  | "audio/wave"
+  | "audio/wav"
+  | "audio/x-wav"
+  | "audio/x-pn-wav"
+  | "audio/webm";
+
+export type ContentType = ContentTypeImage | ContentTypeVideo;
 
 export interface Message {
   sender: string;
